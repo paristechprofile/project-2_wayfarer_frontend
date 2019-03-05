90beee34ae939f7308ba8a6cf53a99ba791771fc
tiffanyteaze <tteaze@gmail.com>
2019-03-05 14:15:40 -0800

    Added signup form to App.js for frontend testing purposes and installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import {
+  Route,
+  Switch
+} from 'react-router-dom'
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
@@ -6,6 +11,18 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Project Wayfarer</h1>
+        <form>
+          <div>
+            <label htmlFor='username'>Username</label>
+            <input type='text' name='username' onChange={this.props.handleInput} />
+          </div>
+
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input type='text' name='password' onChange={this.props.handleInput} />
+          </div>
+          <input value='Submit' type='submit' onClick={this.props.handleSignUp} />
+        </form>
       </div>
     );
   }
