01ad48870e6b87bb9b932242a41d0092afdffde4
tiffanyteaze <tteaze@gmail.com>
2019-03-05 14:35:12 -0800

    Added Walk It Out code to App.js for auth

--- src/App.js
+++ src/App.js
@@ -7,6 +7,82 @@ import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  state = {
+    username: '',
+    pw: '',
+    isLoggedIn: false,
+    user: null
+  }
+
+  componentDidMount () {
+    if (localStorage.token) {
+      axios(
+        { 
+        method: 'get', 
+        url: `http://localhost:3001/user/`, 
+        headers: { authorization: `Bearer ${localStorage.token}` } 
+      })
+      .then( response => {
+        console.log(response)
+          this.setState({
+            isLoggedIn: true,
+            user: response.data
+          })
+      })
+      .catch(err => console.log(err))
+    } else {
+      this.setState({
+        isLoggedIn: false
+      })
+    }
+  }
+
+  handleLogOut = () => {
+    this.setState({
+      email: '',
+      password: '',
+      isLoggedIn: false
+    })
+    localStorage.clear()
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSignUp = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:3001/user/signup', 
+			{ email: this.state.email,
+      	password: this.state.password }
+			)
+      .then( response => {
+        console.log(response)
+        localStorage.token = response.data.signedJwt
+          this.setState({
+            isLoggedIn: true
+          })
+      })
+      .catch(err => console.log(err))
+  }
+
+  handleLogIn = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:3001/user/login', {
+      email: this.state.email,
+      password: this.state.password
+    })
+    .then( response => {
+      localStorage.token = response.data.signedJwt
+      this.setState({
+        isLoggedIn: true
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
