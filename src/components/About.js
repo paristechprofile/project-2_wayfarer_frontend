1bafe58257be2c7d4135747ce99544e06b74ffd2
tiffanyteaze <tteaze@gmail.com>
2019-03-05 23:21:53 -0800

    added content to HomeContainer

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Insert About Us articles for Wayfarer here.</h3>
+      </div>
+    )
+  }
+}
---
+++ src/components/Carousel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Carousel extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Insert carousel here.</h3>
+      </div>
+    )
+  }
+}
