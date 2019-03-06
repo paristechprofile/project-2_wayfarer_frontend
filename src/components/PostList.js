cfc0656be8e12d3930f3a2a477f5110650c2385c
tiffanyteaze <tteaze@gmail.com>
2019-03-05 23:03:17 -0800

    added content to ProfileContainer

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Posts from './Posts'
 
 export default class PostList extends Component {
   render() {
     return (
       <div>
         <h2>User Posts Go Here</h2>
+        <Posts />
       </div>
     )
   }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -5,6 +5,13 @@ export default class UserInfo extends Component {
     return (
       <div>
         <h2>List User Information Here</h2>
+        <img class="temp-profile-pic" src="../../images/blankuserimage.jpg" />
+        <ul className="user-info">
+          <li>Name: firstName lastName</li>
+          <li>City: currentCity</li>
+          <li>Username: username</li>
+        </ul>
+        <button>Edit User Info</button>
       </div>
     )
   }
