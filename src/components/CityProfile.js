35211c07a0839d9e05911043e044f812ae876ad3
nassBouz <bouzianenassima@gmail.com>
2019-03-07 16:42:51 -0800

    src/components/CityProfile.js

--- src/components/CityProfile.js
+++ src/components/CityProfile.js
@@ -1,15 +1,31 @@
+// import React, { Component } from 'react'
+
+// export default class CityProfile extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h2>City Name</h2>
+//         <img className="temp-city-pic" src="../../images/blankcityimage.jpg" alt="Placeholder" />
+//         <button>Create New Post</button>
+//         <div className="city-posts-list">
+//             <h3>Append city posts here.</h3>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
 import React, { Component } from 'react'
+import PostList from '../components/PostList'
+import CreatePost from '../components/CityProfile'
 
 export default class CityProfile extends Component {
   render() {
     return (
       <div>
-        <h2>City Name</h2>
-        <img className="temp-city-pic" src="../../images/blankcityimage.jpg" alt="Placeholder" />
-        <button>Create New Post</button>
-        <div className="city-posts-list">
-            <h3>Append city posts here.</h3>
-        </div>
+        <h1>This is the Cities Profile.</h1>
+        <PostList />
+        {/* <CreatePost /> */}
       </div>
     )
   }
