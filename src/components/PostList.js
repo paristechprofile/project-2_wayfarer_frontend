f75cdca34c0a1b8702862a6d8fde9046e317ccb2
nassBouz <bouzianenassima@gmail.com>
2019-03-07 16:43:39 -0800

    worked on the posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,12 +1,53 @@
+// import React, { Component } from 'react'
+// import Posts from './Posts'
+
+// export default class PostList extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h2>User Posts Go Here</h2>
+//         <Posts />
+//       </div>
+//     )
+//   }
+// }
+
 import React, { Component } from 'react'
+import axios from 'axios'
 import Posts from './Posts'
 
+
+
 export default class PostList extends Component {
+ 
+  state = {
+    posts : []
+    // cityId : '5c80a990d6449e1b62decff4'
+
+  }
+  
+  componentDidMount () {
+    let cityId = '5c80a990d6449e1b62decff4'
+    // router.get('/:id/posts', controllers.cities.getPosts);
+    axios.get(`http://localhost:3001/?id=${cityId}/posts`)
+      .then(response => {
+        this.setState({
+          posts: [response.data]
+        })
+      })
+  }
   render() {
+    const showPosts = this.state.posts.map((post, i) => {
+      return (
+        <div key={i}>
+          <Posts info={post} isLoggedIn={this.props.isLoggedIn} />
+        </div>
+      )
+      console.log("wwwwwwwwwwwwwwwwww")
+    })
     return (
       <div>
-        <h2>User Posts Go Here</h2>
-        <Posts />
+        {showPosts}
       </div>
     )
   }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,40 @@
+// import React, { Component } from 'react'
+
+// export default class Posts extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h3>Append Post divs here.</h3>
+//       </div>
+//     )
+//   }
+// }
+
+
 import React, { Component } from 'react'
 
-export default class Posts extends Component {
-  render() {
+class Posts extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      post: this.props.info
+    }
+  }
+
+  render () {
     return (
-      <div>
-        <h3>Append Post divs here.</h3>
+      <div className='post-preview'>
+        <h3>{this.state.post.author}</h3>
+        <h3>{this.state.post.date}</h3>
+        <p>{this.state.post.text}</p>
+        {/* {this.props.isLoggedIn ? <h4>Age: {this.state.dog.age}</h4> : ''}
+        {this.props.isLoggedIn ? <h4>Walk Frequency: {this.state.dog.frequency}</h4> : ''} */}
       </div>
     )
   }
 }
+
+export default Posts
+
+
